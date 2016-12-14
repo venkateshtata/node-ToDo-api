c63a23ccd4c343621e021822acad8fc4f6ba03e2
venkatesh <venkateshtata9@gmail.com>
2016-12-14 12:27:52 +0530

    deploying on heroku

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ var {User} = require('./models/user');
 
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -66,9 +67,9 @@ app.get('/todos/:id',(req,res)=>{
 //   });
 // });
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
 
-  console.log("listening to port 3000");
+  console.log(`listening on ${port}`);
 
 });
 
