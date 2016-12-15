ec776de76783ccf03297be571c68f35aa630ba03
venkatesh <venkateshtata9@gmail.com>
2016-12-15 08:39:34 +0530

    add new routes

--- server/server.js
+++ server/server.js
@@ -25,6 +25,18 @@ app.post('/todos',(req, res)=>{
     res.status(400).send(err);
   });
 });
+
+app.post('/todos',(req, res)=>{
+  var todo = new Todo({
+    text:req.body.text
+  });
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(err)=>{
+    res.status(400).send(err);
+  });
+});
+
 app.get('/todos/:id',(req,res)=>{
    var id = req.params.id;
 
